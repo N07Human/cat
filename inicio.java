e00f34f68a0856a431d3d4879ac2434c096f7e04
N07Human <juanpablo.saavedrag@gmail.com>
2017-08-22 00:37:18 -0300

    Update inicio.java

--- inicio.java
+++ inicio.java
@@ -1,5 +1,11 @@
 package juego;
 import java.io.*;
   public static void main(Strings[]args) throws IOException{
-    
+    String posicion[][] = new String[3][3];
+    BufferedReader input = new BufferedReader(new InputStreamReader(system.in));
+    for(int i=0;i<posicion[0].lenght;i++){
+      for(j=0;j<posicion[0][0].length;j++){
+        posicion[i][j]="-";
+      }
+      }
   }
